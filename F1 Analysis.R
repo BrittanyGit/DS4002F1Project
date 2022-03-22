2200c917603f8c50eb0e06aff5bea8cfa4d74863
Connor Nickol <cnickol26@gmail.com>
2022-03-22 16:44:16 -0400

    Add files via upload

--- F1 Analysis.R
+++ F1 Analysis.R
@@ -1,5 +1,9 @@
+#Connor Nickol
+
+#Load packages
 library(tidyverse)
 
+#Read in data sets
 circuits<-read.csv("circuits.csv")
 con_results<-read.csv("constructor_results.csv")
 con_standings<-read.csv("constructor_standings.csv")
@@ -13,6 +17,7 @@ results<-read.csv("results.csv")
 seasons<-read.csv("seasons.csv")
 status<-read.csv("status.csv")
 
+#Remove columns from certain sets
 circuits2<-circuits %>% select(-c(location,circuitRef,url))
 drivers2<-drivers %>% select(-c(driverRef,number,dob,url))
 races2<-races %>% select(-c(date,time,url))
@@ -20,8 +25,11 @@ results2<-results %>% select(-c(constructorId,number,positionText,
                                 points,laps,time,milliseconds,
                                 fastestLap,fastestLapTime,fastestLapSpeed,
                                 rank))
+
+#Remove columns and filter for qualifying position to be 3 or less
 qualifying2<- qualifying %>% select(-c(qualifyId,constructorId,number)) %>% filter(position <= 3)
 
+#Fix qualifying times and convert to be 0 if not present
 qualifying2$q1<-gsub('[\\N]'," ",qualifying2$q1)
 qualifying2$q2<-gsub('[\\N]'," ",qualifying2$q2)
 qualifying2$q3<-gsub('[\\N]'," ",qualifying2$q3)
@@ -32,26 +40,33 @@ qualifying2$q1[qualifying2$q1==""]<-0
 qualifying2$q2[qualifying2$q2==""]<-0
 qualifying2$q3[qualifying2$q3==""]<-0
 
+#Put all qualifying times into q1
 for (i in 1:nrow(qualifying2)){
   if(qualifying2$q3[i]==0){
     qualifying2$q3[i]<-qualifying2$q1[i]
   }
 }
 
+#Remove q2 and q3 columns 
 qualifying2<-qualifying2 %>% filter(q3 != 0) %>% select(-c(q1,q2))
 
+#Rename some variables
 qualifying2<-rename(qualifying2, qual_time=q3)
 qualifying2<-rename(qualifying2, qual_positon=position)
 results2<-rename(results2, finish_position=position)
 circuits2<-rename(circuits2, circuit=name)
 
+#Get number of pit stops for each driver each racce
 pit_stops2<-pit_stops %>% group_by(raceId,driverId) %>% 
   arrange(desc(stop)) %>% slice(1)
 
+#Rename column
 pit_stops2<-rename(pit_stops2, num_stops=stop)
 
+#Remove extra columns
 pit_stops2<-pit_stops2 %>% select(-c(lap,time,duration,milliseconds))
 
+#Join together filtered data sets
 formula_1_data<-qualifying2 %>% left_join(results2,by=c("raceId","driverId")) 
 
 formula_1_data<-formula_1_data %>% filter(finish_position != "\\N")
@@ -61,5 +76,51 @@ formula_1_data<-formula_1_data %>% left_join(races2,by=c("raceId"))
 formula_1_data<-formula_1_data %>% left_join(circuits2,by=c("circuitId"))
 
 formula_1_data<-formula_1_data %>% left_join(drivers2,by=c("driverId"))
+
+write.csv(formula_1_data,"f1_data.csv",row.names = FALSE)
+
+#Create qual qap and won race dummy variables
+formula_1_data<-formula_1_data %>% mutate(qual_gap=0,won_race=0)
+
+# Set won race to be a 1 if driver won the race
+for(i in 1:nrow(formula_1_data)){
+  if (formula_1_data$finish_position[i]==1){
+    formula_1_data$won_race[i]<-1
+  }
+}
+
+#Remove colons from qual time
+formula_1_data$qual_time <- gsub("[:]", "" ,formula_1_data$qual_time, perl=TRUE)
+
+#Make qual time a numeric
+formula_1_data<-formula_1_data %>% mutate(qual_time=as.numeric(qual_time))
+
+#Convert qual times to be in seconds
+for (i in 1:nrow(formula_1_data)){
+  if (formula_1_data$qual_time[i] >=100 & formula_1_data$qual_time[i] < 200){
+    formula_1_data$qual_time[i]<-60+(formula_1_data$qual_time[i]-100)
+  }
+  else if (formula_1_data$qual_time[i] >=200 & formula_1_data$qual_time[i] < 300){
+    formula_1_data$qual_time[i]<-120+(formula_1_data$qual_time[i]-200)
+  }
+  else{
+    formula_1_data$qual_time[i]<-formula_1_data$qual_time[i]
+  }
+}
+
+#Arrange by raceid least to greatest
+formula_1_data<-formula_1_data %>% arrange(raceId)
+
+#create time global variable
+time<-0
+
+#Create qual gap times 
+for (i in 1:nrow(formula_1_data)){
+  ifelse(formula_1_data$qual_positon[i]==1,
+         time<-formula_1_data$qual_time[i],
+         formula_1_data$qual_gap[i]<-formula_1_data$qual_time[i]-time) 
+}
+
+
 
 
